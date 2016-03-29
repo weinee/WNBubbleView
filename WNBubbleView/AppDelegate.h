28037411b920fe6d07787f67a4f733fc540b2bc3
LiJF_weinee <LiJF_wn@163.com>
2016-03-29 22:59:59 +0800

    Initial Commit

---
+++ WNBubbleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WNBubbleView
+//
+//  Created by weinee on 16/3/29.
+//  Copyright © 2016年 weinee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WNBubbleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WNBubbleView
+//
+//  Created by weinee on 16/3/29.
+//  Copyright © 2016年 weinee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
